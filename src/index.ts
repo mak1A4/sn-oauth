6b0b9531401a0367c38a55784e98a36821cc3d9c
mak1A4 <m.kirchweger@protonmail.com>
2021-07-26 01:04:37 +0200

    intial commit

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import * as chalk from 'chalk';
+import * as inquirer from 'inquirer';
+import { QuestionCollection } from "inquirer";
+import oauth from "./oauth";
+
+interface cliReturnType {
+  accessToken: string, refreshToken: string
+}
+
+export default async function (instance?: string, clientId?: string): Promise<cliReturnType> {
+
+  console.log(chalk.green("ServiceNow OAuth 2.0 Authentication").bold());
+  console.log(chalk.blueBright(`To get a Client ID & Client Secret you have to create
+  an OAuth Application Registry record in ServiceNow. You just have to provide a name, 
+  you can leave everything else blank.`));
+  console.log(chalk.yellow("======================================="));
+  
+  let questionList: Array<QuestionCollection> = [];
+  if (!instance) {
+    questionList.push({
+      name: "instanceName", type: "input", message: "Instance Name:"
+    });
+  }
+  if (!clientId) {
+    questionList.push({
+      name: "clientId", type: "input", message: "Client ID:"
+    });
+  }
+  questionList.push({
+    name: "user", type: "input", message: "User:"
+  });
+  questionList.push({
+    name: "userPassword", type: "password", message: "Password:", mask: "*"
+  });
+  questionList.push({
+    name: "clientSecret", type: "password", message: "Client Secret:", mask: "*"
+  });
+
+  let answers = await inquirer.prompt(questionList);
+  if (!instance) instance = answers.instanceName;
+  if (!clientId) clientId = answers.clientId;
+
+  let oauthInstance = oauth(instance);
+  let refreshToken = await oauthInstance.getRefreshToken
+    (answers.user, answers.userPassword, clientId, answers.clientSecret);
+  let accessToken = await oauthInstance.getAccessToken
+    (clientId, answers.clientSecret, refreshToken);
+
+  return { "accessToken": accessToken, "refreshToken": refreshToken };
+};
---
+++ src/oauth.ts
@@ -0,0 +1,65 @@
+import axios from "axios";
+import * as qs from 'qs'
+
+interface getRefreshTokenFunc {
+  (username: string, password: string, clientId: string, clientSecret: string): Promise<string>;
+}
+interface getAccessTokenFunc {
+  (clientId: string, clientSecret: string, refreshToken: string): Promise<string>
+}
+interface oauthReturn {
+  getRefreshToken: getRefreshTokenFunc, getAccessToken: getAccessTokenFunc
+}
+
+export default function oauth(instance: string): oauthReturn {
+
+  let instanceUrl: string = `https://${instance}.service-now.com`;
+  let oauthTokenUrl: string = `${instanceUrl}/oauth_token.do`;
+  let headers: object = {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+  };
+
+  async function getRefreshToken(
+    username: string,
+    password: string,
+    clientId: string,
+    clientSecret: string): Promise<string> {
+
+      console.log("username: " + username);
+      console.log("password: " + password);
+      console.log("clientId: " + clientId);
+      console.log("clientSecret: " + clientSecret);
+
+    var response = await axios.post(oauthTokenUrl, qs.stringify({
+      "grant_type": "password",
+      "client_id": clientId,
+      "client_secret": clientSecret,
+      "username": username,
+      "password": password
+    }), {
+      headers: headers
+    });
+    return response.data.refresh_token;
+  };
+
+  async function getAccessToken(
+    clientId: string,
+    clientSecret: string,
+    refreshToken: string): Promise<string> {
+
+    var response = await axios.post(oauthTokenUrl, qs.stringify({
+      "grant_type": "refresh_token",
+      "client_id": clientId,
+      "client_secret": clientSecret,
+      "refresh_token": refreshToken
+    }), {
+      headers: headers
+    });
+    return response.data.access_token;
+  }
+
+  return {
+    "getRefreshToken": getRefreshToken,
+    "getAccessToken": getAccessToken
+  };
+}
